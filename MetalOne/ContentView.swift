22986908ef04a98ec5fc962ef0adfb50f1cfc464
Mastour2 <5wiled.ur@gmail.com>
2026-01-30 21:58:52 +0300

    Initial Commit

---
+++ MetalOne/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MetalOne
+//
+//  Created by Khalid Mastour on 11/08/1447 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MetalOne/MetalOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalOneApp.swift
+//  MetalOne
+//
+//  Created by Khalid Mastour on 11/08/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct MetalOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
